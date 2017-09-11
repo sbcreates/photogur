dbfc6d6dfa0b9194d2e3c314e472d154f2e9694b
sbcreates <drawsarahb@gmail.com>
2017-09-11 12:36:45 -0400

    Created many to one relationship between pictures and users. Added user_id to pictures table

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,7 +1,5 @@
 class Picture < ApplicationRecord
 
-# If you find yourself using the same code over and over again to select records, you’ll want to save those queries as methods in your model.
-
   # validates artist and url input is present
   validates :artist, :url, presence: true #{ message: "artist must be given"}
   # validates title length is between 3-20 char long
@@ -9,6 +7,7 @@ class Picture < ApplicationRecord
   # validates url is unique
   validates :url, uniqueness: true
 
+  belongs_to :user
 
   def self.newest_first
   Picture.order("created_at DESC")
