30fda78a9bf6a8fe39abc4ffa9ba4f037086777a
sbcreates <drawsarahb@gmail.com>
2017-09-18 16:45:25 -0400

    Fixed logout error

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
 
   root 'pictures#index'
+
+  get 'login' => 'sessions#new', :as => :login
+
+  delete 'logout' => 'sessions#destroy', :as => :logout
+
   resources :pictures
   resource :users, only: %i(create new edit update)
-  resource :sessions, only: [:create, :new, :destroy]
+  resource :sessions, only: [:create]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
